0264616a2fa88b38fe6c2344f64c7deae80e0dc5
ndto <152689844+ndto@users.noreply.github.com>
2025-03-29 02:25:28 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+let songs = [
+    {
+        name: "TTM",
+        path: "musics/Song 1.mp3",
+        artist: "Tombel Fvnky Rmx",
+        cover: "images/cover 1.png"
+    },
+    {
+        name: "DJ Mengkane",
+        path: "musics/Song 2.mp3",
+        artist: "DJ Tebang",
+        cover: "images/cover 2.png"
+    }
+]
